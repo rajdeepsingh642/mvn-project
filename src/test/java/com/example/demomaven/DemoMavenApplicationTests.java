5c01268f2af652762f74d35ad015e373f4af19fc
rajdeepsingh642 <rajdeepsingh642@gmail.com>
2024-04-23 11:20:55 +0530

    all

---
+++ src/test/java/com/example/demomaven/DemoMavenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demomaven;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DemoMavenApplicationTests {
+	
+		public static void main(String []args)
+		{
+			System.out.println("My First Java Program.");
+		}
+	}
+	
+
+
